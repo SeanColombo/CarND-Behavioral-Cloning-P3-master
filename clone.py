c22ef0d763c93d464f67b978c68414b56007754b
Sean Colombo <sean.colombo@gmail.com>
2017-05-07 20:02:59 -0400

    Now using NVidia Architecture and cropping out the top and bottom of images. When training it on my long training data (2 laps in each direction) it can get over the bridge but then gets caught by the little 'shortcut' offroad spot with the tire stacks.

--- clone.py
+++ clone.py
@@ -2,8 +2,25 @@ import csv
 import cv2
 import numpy as np
 
+####
+# NOTES: I didn't do the flipping images for data augmentation because I drove my car around the track twice
+# in each direction, so there shouldn't be a directional bias.
+#
+# TODO: Consider doing the trick with the multiple cameras & tweaked steering angles (Lesson 12-12).
+# TODO: Do some "recovery" sections (when heading around the track in either direction)... only record the correction, not the initial driving to the edge.
+# TODO: Use track 2?  This will make it more general-purpose but is probably only good for after it can drive track 1.
+####
+
+
 #INPUT_DIR = "data" # DATA THAT COMES WITH THE PROJECT
-INPUT_DIR = "seandata" # Me, driving around
+#INPUT_DIR = "seandata" # Me, driving around, one lap.
+INPUT_DIR = "seandata_long" # Me, driving twice in each direction. This removes the bias for turning left.
+# Current Data in seandata_long:
+#   Lap
+#   Backwards Lap
+#   Lap
+#   Backwards Lap
+
 
 lines = []
 with open("./"+INPUT_DIR+"/driving_log.csv") as csvfile:
@@ -31,17 +48,45 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten,Dense,Lambda
+from keras.layers import Flatten,Dense,Lambda,Convolution2D,Cropping2D
+#from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
 model = Sequential()
+
+# == PREPROCESSING ==
+# Normalize the pixels to be -0.5 to 0.5.
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+# Crop out the top and bottom  pixels which are sky or car-hood.
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+
+# == MODEL ==
+# Roughly, a LeNet architecture. This has really low loss but still doesn't drive well.
+# model.add(Convolution2D(6,5,5,activation="relu"))
+# model.add(MaxPooling2D())
+# model.add(Convolution2D(6,5,5,activation="relu"))
+# model.add(MaxPooling2D())
+# model.add(Flatten())
+# model.add(Dense(120))
+# model.add(Dense(84))
+# model.add(Dense(1))
+
+# The NVIdia architecture
+model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
+
 
 model.compile(loss='mse', optimizer='adam')
 # Default data:
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-# My early data (before the Lambda layer) seemed to continue learning for about 20 epochs
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 output_file = 'model_'+INPUT_DIR+'.h5'
 model.save(output_file)
